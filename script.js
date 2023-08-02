f97cef4d2125ab1d689f24740ad4edd709f03a8c
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-02 11:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+const images = Array.from(document.getElementByTagName("img"));
+let count = 0;
+let arr = [];
+const reset = document.getElement1ById("reset");
+const verify = document.getElement1ById("verify");
+images.forEach((img)=>{
+	img.addEventListener(("click"),(e)=>{
+		reset.style.visiblity = "visible";
+		count++;
+		arr.push(img.ClassName);
+		if(arr.length>=2 && e.ClassName!==arr[0]){
+			verify.style.visiblity = "visible";
+		}
+	})
+})
